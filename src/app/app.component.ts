a14a2363ef85b4444720d8218513bb8270d038d0
viggy <dviglione02@gmail.com>
2020-03-30 15:18:04 -0400

    angular dropdown does not show default value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'select-bug';
+  collection: string[] = ['Option 1', 'Option 2'];
+
+  ngOnInit(){
+    console.log('and the collection: ', this.collection);
+  }
 }
---
+++ src/app/select-box/select-box.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-select-box',
+  templateUrl: './select-box.component.html',
+  styleUrls: ['./select-box.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectBoxComponent),
+      multi: true
+    }
+  ]
+})
+export class SelectBoxComponent implements ControlValueAccessor {
+  value: string;
+
+  @Input()
+  options: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('and the specified options: ', this.options); 
+  }
+
+  onChange: () => void;
+  onTouch: () => void;
+  disabled: boolean;
+
+  writeValue(value: string): void {
+    if(value){
+      this.value = value;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
